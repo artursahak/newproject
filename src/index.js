5f6b257ce9d87d89a41811e11a7f88b6f492bb70
artursahak <arthursahakyan99@gmail.com>
2019-12-17 15:22:56 +0400

    my message

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 class Car extends React.Component{
 	constructor() {
 		super();
-		this.state={color:"red"};
+		this.state={color:"red",color2:"blue"};
 	}
 	render() {
-		return <h2>Hi, I am a {this.state.color} Car!</h2>;
+		return <h2>Hi, I am a {this.state.color2} Car!</h2>;
 	}
 }
 
